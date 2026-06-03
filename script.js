07f8e06784a3552e300e6bc2b06fbb8b614e458c
tamilselvan <tamilselvan.arumugam.a@gmail.com>
2026-06-03 15:10:29 +0530

    Weight Converter Project

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const hour = document.getElementById("hour");
-const minute = document.getElementById("minute");
-const second = document.getElementById("second");
+const input = document.getElementById("input");
+const result = document.getElementById("result");
 
-function setClock() {
-    const now = new Date();
+input.addEventListener("input", () => {
+    let pounds = Number(input.value);
 
-    const sec = now.getSeconds();
-    const min = now.getMinutes();
-    const hr = now.getHours();
-
-    second.style.transform = `translateX(-50%) rotate(${sec * 6}deg)`;
-    minute.style.transform = `translateX(-50%) rotate(${min * 6}deg)`;
-    hour.style.transform =
-      `translateX(-50%) rotate(${hr * 30 + min * 0.5}deg)`;
-}
-
-setInterval(setClock, 1000);
-setClock();
+    if (pounds === 0 || input.value === "") {
+        result.innerHTML = "0";
+    } else {
+        let kg = pounds * 0.453592;
+        result.innerHTML = kg.toFixed(2) + " Kg";
+    }
+});
