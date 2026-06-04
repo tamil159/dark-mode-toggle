e13ccf6fa17fd4ce519352cb197739ed85fbef24
tamilselvan <tamilselvan.arumugam.a@gmail.com>
2026-06-04 12:11:27 +0530

    Dark Mode Toggle Project

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-const input = document.getElementById("input");
-const result = document.getElementById("result");
+const toggle = document.getElementById("toggle");
 
-input.addEventListener("input", () => {
-    let pounds = Number(input.value);
-
-    if (pounds === 0 || input.value === "") {
-        result.innerHTML = "0";
-    } else {
-        let kg = pounds * 0.453592;
-        result.innerHTML = kg.toFixed(2) + " Kg";
-    }
+toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
 });
